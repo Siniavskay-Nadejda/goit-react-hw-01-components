7cfc058d0718dbdb891c525ea75a3adcb4497829
Siniavskay-Nadejda <87280720+Siniavskay-Nadejda@users.noreply.github.com>
2022-02-15 11:31:12 +0200

    компонент

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,13 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
+import user from "../user.json"
+import Profile from "components/Profile"
+export default function App () {
+  return (<Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+  /> 
+)
+ 
 };
